b0159bc63bb12ce46ecb4351bff86d60b0d88b93
EchoX_roblox <carloseduardosilvaesilva934@gmail.com>
2026-07-04 16:01:43 -0300

    Update Supabase key to a new value

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,5 @@
 const SUPABASE_URL = "https://wiosfrpgbgzqqiheaxve.supabase.co";
-const SUPABASE_KEY = "sb_publishable_YD_smd2JMrjDNVryI7J5WQ_GPrXYx3H";
+
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indpb3NmcnBnYmd6cXFpaGVheHZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNzQ1MTUsImV4cCI6MjA5ODc1MDUxNX0.ngj64yzE2IuC8dKZen-TA5WJl6IA3_d7BST4lKY8eVk";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
