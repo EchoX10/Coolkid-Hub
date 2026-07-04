39a7ef5a28b20ed49ecd85622fec3f436619b51b
EchoX_roblox <carloseduardosilvaesilva934@gmail.com>
2026-07-04 15:27:27 -0300

    Initialize Supabase client with URL and key

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,4 @@
+const SUPABASE_URL = "https://wiosfrpgbgzqqiheaxve.supabase.co";
+const SUPABASE_KEY = "sb_publishable_YD_smd2JMrjDNVryI7J5WQ_GPrXYx3H";
 
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
